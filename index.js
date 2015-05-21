23c5ba74803ef327160387664e3ceb00005ca45f
Jeremy Ellis <jellis@hpssapps.com>
2015-05-21 12:38:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,17 +9,45 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
+
+
+
+
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+
+
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
+
+
+
+
+
+
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+        console.log("send ping")
+    //ws.send(JSON.stringify(new Date()), function() {  })
+      ws.send("B",function() {  })
+    
+  }, 4000)
 
-  console.log("websocket connection open")
+  console.log("websocket connection open " )
+  
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+         wss.broadcast(message);  
+        });  
+        
+
+  
 
   ws.on("close", function() {
     console.log("websocket connection close")
