c5213ff7538db80638e728d4d97ea75a806f22ea
Jeremy Ellis <jellis@hpssapps.com>
2015-05-21 14:42:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
 var app = express()
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
 server.listen(port)
-
-
-
 
 
 console.log("http server listening on %d", port)
@@ -19,38 +16,35 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 
-
-wss.broadcast = function(data) {
-  for (var i in this.clients)
-    this.clients[i].send(data);
+wss.myBroadcast = function(data) {
+  for (var i in this.clients){
+     this.clients[i].send(data);
+     console.log('sent to client[' + i + '] ' + data);
+  }
 };
-
-
-
-
-
 
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-        console.log("send ping")
-    //ws.send(JSON.stringify(new Date()), function() {  })
-      ws.send("B",function() {  })
+      console.log("send ping: C240")
+      //ws.send(JSON.stringify(new Date()), function() {  })
+      ws.send("C240",function() {  })
     
-  }, 4000)
-
-  console.log("websocket connection open " )
+  }, 8000)   
   
-    ws.on('message', function(message) {
-        console.log('received: %s', message);
-         wss.broadcast(message);  
-        });  
+  
+  console.log("websocket connection open " + this.clients[0].IP )
+  
+  ws.on('message', function(message) {
+      console.log('received: %s', message);
+      wss.myBroadcast(message);  
+  });  
         
 
-  
-
   ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
+     console.log("websocket connection close")
+     clearInterval(id)
   })
-})
+  
+  
+})  // end wss.on
