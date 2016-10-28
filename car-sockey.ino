20f1a2d320fd157bc45a1be698cb1259b4fd8291
Jeremy Ellis <keyfreemusic@gmail.com>
2016-10-28 13:50:47 -0700

    Create car-sockey.ino

---
+++ car-sockey.ino
@@ -0,0 +1,183 @@
+TCPClient client;
+
+
+char server[] = "socket01-e2teacher.c9users.io";
+
+
+bool myUsbSerialDebugOn = false;      // set to false when not hooked up to USB
+
+int mySafetyCount = 0;
+
+int myLeftMotorDirection  = 1;  // 1 forward, 2 backward
+int myRightMotorDirection = 1;  // 1 forward, 2 backward
+int myBrake = 0; // 0 no brake, 1 coast, 2 power brake;
+
+int myLeftMotorSpeed  = 0;  //0 stop, 255 fastest
+int myRightMotorSpeed = 0;  //0 stop, 255 fastest
+
+void mySetMotors(){
+    digitalWrite(D7, HIGH);
+    mySafetyCount = 0;
+    if (myLeftMotorDirection  == 1){ digitalWrite(D3, 1); digitalWrite(D4, 0);} //left forward
+    if (myLeftMotorDirection  == 2){ digitalWrite(D3, 0); digitalWrite(D4, 1);} 
+   
+    if (myRightMotorDirection  == 1){ digitalWrite(D5, 1); digitalWrite(D6, 0);} // right forward
+    if (myRightMotorDirection  == 2){ digitalWrite(D5, 0); digitalWrite(D6, 1);}  
+   
+   
+   
+    if (myBrake == 1) {digitalWrite(D3, 0); digitalWrite(D4, 0); digitalWrite(D5, 0); digitalWrite(D6, 0);} // coast
+    if (myBrake == 2) {digitalWrite(D3, 1); digitalWrite(D4, 1); digitalWrite(D5, 1); digitalWrite(D6, 1);} // power brake
+   
+   
+   
+
+    if (myLeftMotorSpeed < 0   ){myLeftMotorSpeed = 0;   } // limits
+    if (myLeftMotorSpeed > 255 ){myLeftMotorSpeed = 255; } 
+    
+    if (myRightMotorSpeed < 0  ){myRightMotorSpeed = 0;  }
+    if (myRightMotorSpeed > 255){myRightMotorSpeed = 255;}
+    
+    analogWrite(A4, myLeftMotorSpeed); // set motor speeds
+    analogWrite(A5, myRightMotorSpeed);
+    delay(3);
+    digitalWrite(D7, LOW);
+}
+
+
+
+
+int connectToMyServer(String myNothing) {
+
+  digitalWrite(D7, HIGH);
+  if (client.connect(server, 80)) {
+      client.write("GET / HTTP/1.1\r\n");
+      client.write("Host: socket01-e2teacher.c9users.io\r\n");
+      client.write("Upgrade: websocket\r\n");
+      client.write("Connection: Upgrade\r\n");
+      client.write("Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n");
+      client.write("Sec-WebSocket-Version: 13\r\n");
+      client.write("\r\n");
+      
+      // set the motors
+      myBrake=0; 
+      myLeftMotorDirection=1; 
+      myRightMotorDirection=1; 
+      myLeftMotorSpeed=0; 
+      myRightMotorSpeed=0;
+      mySetMotors();
+      digitalWrite(D7, LOW);
+      if (myUsbSerialDebugOn){
+         Serial.println("successfully connected");
+       }
+      return 1; // successfully connected
+  } else {
+     if (myUsbSerialDebugOn){
+         Serial.println("failed to connect");
+      }
+     return -1; // failed to connect
+  }
+
+}
+
+
+
+int stopMyServer(String myNothing) {
+
+    digitalWrite(D7, HIGH);
+
+    while(client.read() >= 0);    // ignore the rest of the http request
+    client.stop();                // shut down the client for next connection
+
+    digitalWrite(D7, LOW);
+    if (myUsbSerialDebugOn){
+       Serial.println("successfully disconnected");
+    }
+    return 1;
+
+}
+
+
+void setup() {
+
+      pinMode(D7, OUTPUT);
+      
+      pinMode(D3, OUTPUT); // left
+      pinMode(D4, OUTPUT); // left
+      pinMode(D5, OUTPUT); // right
+      pinMode(D6, OUTPUT); // right
+      
+      pinMode(A4, OUTPUT); // left
+      pinMode(A5, OUTPUT); // right
+
+      Spark.function("connect", connectToMyServer);
+      Spark.function("stop", stopMyServer);
+      digitalWrite(D7, HIGH);
+      delay(25000);
+      digitalWrite(D7, LOW);
+      
+      
+    if (myUsbSerialDebugOn){
+       Serial.begin(9600);             // Initalize the USB Serial port
+       while(!Serial.available()) SPARK_WLAN_Loop();
+       Serial.println("Hello Computer");
+    }
+
+}
+
+void loop() {
+
+   mySafetyCount += 1;
+
+   if (mySafetyCount >= 4000){  // no connection for a while stop motors
+        mySafetyCount = 0;
+        myLeftMotorSpeed = 0;
+        myRightMotorSpeed = 0;
+        mySetMotors();
+    }
+  
+  if (client.connected()) {
+    if (client.available()) {
+       
+
+        
+        char myIncoming = client.read();
+
+        
+        
+        if (myIncoming == 'A'){ digitalWrite(D7, HIGH);}
+        if (myIncoming == 'B'){ digitalWrite(D7, LOW);}
+        if (myIncoming == 'C'){ RGB.brightness(5); }
+        if (myIncoming == 'D'){ RGB.brightness(100); }
+        if (myIncoming == 'E'){ RGB.brightness(250); }
+        if (myIncoming == 'F'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=2; myLeftMotorSpeed=50; myRightMotorSpeed=50; mySetMotors(); }  // rotate right
+        if (myIncoming == 'G'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=1; myLeftMotorSpeed=50; myRightMotorSpeed=50; mySetMotors(); }  // rotate left
+
+        if (myIncoming == 'H'){ myLeftMotorSpeed=0; myRightMotorSpeed=0; mySetMotors(); }  // stop
+        if (myIncoming == 'I'){ myLeftMotorSpeed=50; myRightMotorSpeed=50; mySetMotors(); }  // slow
+        if (myIncoming == 'J'){ myLeftMotorSpeed=100; myRightMotorSpeed=100; mySetMotors(); }  // OK
+        if (myIncoming == 'K'){ myLeftMotorSpeed=150; myRightMotorSpeed=150; mySetMotors(); }  // medium
+
+        if (myIncoming == 'L'){ myLeftMotorSpeed=200; myRightMotorSpeed=200; mySetMotors(); }  // fast
+        if (myIncoming == 'M'){ myLeftMotorSpeed=255; myRightMotorSpeed=255; mySetMotors(); }  // fastest
+
+        if (myIncoming == 'Q'){ myBrake=0; myLeftMotorSpeed+=10; myRightMotorSpeed-=10; mySetMotors(); }  // left
+        if (myIncoming == 'R'){ myBrake=0; myLeftMotorSpeed+=50; myRightMotorSpeed-=50; mySetMotors(); }  // big left
+        if (myIncoming == 'S'){ myBrake=0; myLeftMotorSpeed-=10; myRightMotorSpeed+=10; mySetMotors(); }  // right
+        if (myIncoming == 'T'){ myBrake=0; myLeftMotorSpeed-=50; myRightMotorSpeed+=50; mySetMotors(); }  // big right
+
+        if (myIncoming == 'U'){ myBrake=1; mySetMotors(); }  // coast
+        if (myIncoming == 'V'){ myBrake=2; mySetMotors(); }  // power break
+        if (myIncoming == 'W'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=1; mySetMotors(); }  // set forward
+        if (myIncoming == 'X'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=2; mySetMotors(); }  // set backward
+     
+        
+        if (myUsbSerialDebugOn){
+            Serial.print(myIncoming);
+        }  
+  
+
+      
+      }   
+    }
+}
