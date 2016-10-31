b9fdb9e122f9d06acccbebcb9c43decf2266d998
Jeremy Ellis <keyfreemusic@gmail.com>
2016-10-31 14:18:56 -0700

    Update car-socket.ino

--- car-socket.ino
+++ car-socket.ino
@@ -1,10 +1,13 @@
+
 TCPClient client;
+
 
 
 char server[] = "socket01-e2teacher.c9users.io";  // set again later!!!
 
 
 bool myUsbSerialDebugOn = false;      // set to false when not hooked up to USB
+bool myGoNow = false;      // set to false, true to stop motors
 
 int mySafetyCount = 0;
 
@@ -50,25 +53,27 @@ void mySetMotors(){
 int connectToMyServer(String myNothing) {
 
   digitalWrite(D7, HIGH);
-  mySafetyCount = 4001;
 
-
-  
   
   if (client.connect(server, 80)) {
       client.write("GET / HTTP/1.1\r\n");
-      client.write("Host: socket01-e2teacher.c9users.io\r\n");  // note easiest to copy server here
+     // client.write("Host: socket01-e2teacher.c9users.io\r\n");  // note easiest to copy server here
+      client.write("Host: "+String(server)+"\r\n");  
       client.write("Upgrade: websocket\r\n");
       client.write("Connection: Upgrade\r\n");
-      client.write("Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n");
+     // client.write("Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n");
+      client.write("Sec-WebSocket-Key: 444444444444==\r\n");
       client.write("Sec-WebSocket-Version: 13\r\n");
       client.write("\r\n");
-      
+
 
       if (myUsbSerialDebugOn){
          Serial.println("successfully connected");
        }
+       
+      myGoNow = false;
       return 1; // successfully connected
+
   } else {
      if (myUsbSerialDebugOn){
          Serial.println("failed to connect");
@@ -87,7 +92,7 @@ int stopMyServer(String myNothing) {
 
     digitalWrite(D7, HIGH);
     mySafetyCount = 4001; 
-    
+    myGoNow = false;
     while(client.read() >= 0);    // ignore the rest of the http request
     client.stop();                // shut down the client for next connection
     
@@ -97,13 +102,51 @@ int stopMyServer(String myNothing) {
     if (myUsbSerialDebugOn){
        Serial.println("successfully disconnected");
     }
+    
+    
+    
     return 1;
     
     
-        
+    
 
     
 }
+
+
+
+
+
+int myGo(String myNothing) {
+
+    digitalWrite(D7, HIGH);
+    myGoNow = true;
+    
+    while(client.read() >= 0){};    // ignore the rest of the http request
+
+    myBrake=0;    // reset everything
+    myLeftMotorDirection=1; 
+    myRightMotorDirection=1;
+    myLeftMotorSpeed = 0;
+    myRightMotorSpeed = 0;
+    mySetMotors();
+    mySafetyCount = 0;
+    
+    delay(5);
+    digitalWrite(D7, LOW);
+    return 1;
+
+}
+
+int myNotGo(String myNothing) {
+
+    digitalWrite(D7, HIGH);
+    myGoNow = false;
+    delay(50);
+    digitalWrite(D7, LOW);
+    return 1;
+}
+
 
 
 void setup() {
@@ -118,8 +161,10 @@ void setup() {
       pinMode(A4, OUTPUT); // left analog
       pinMode(A5, OUTPUT); // right analog
 
-      Spark.function("connect", connectToMyServer);
-      Spark.function("stop", stopMyServer);
+      Particle.function("connect", connectToMyServer);
+      Particle.function("stop", stopMyServer);
+      Particle.function("myGo1", myGo);
+      Particle.function("myNotGo1", myNotGo);
       digitalWrite(D7, HIGH);
       delay(25000);
       digitalWrite(D7, LOW);
@@ -134,18 +179,23 @@ void setup() {
 }
 
 void loop() {
+ 
 
    mySafetyCount += 1;
+   
 
-   if (mySafetyCount >= 4000){  // no connection for a while stop motors
-        mySafetyCount = 0;
+   if (mySafetyCount >= 1500){  // no connection for a while stop motors
+        myBrake=0; 
+        myLeftMotorDirection=1; 
+        myRightMotorDirection=1;
         myLeftMotorSpeed = 0;
         myRightMotorSpeed = 0;
         mySetMotors();
+        mySafetyCount = 0;
     }
-  
-  if (client.connected()) {
-    if (client.available()) {
+  if (myGoNow){
+    if (client.connected()) {
+      if (client.available()) {
        
 
         
@@ -158,26 +208,32 @@ void loop() {
         if (myIncoming == 'C'){ RGB.brightness(5); }
         if (myIncoming == 'D'){ RGB.brightness(100); }
         if (myIncoming == 'E'){ RGB.brightness(250); }
-        if (myIncoming == 'F'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=2; myLeftMotorSpeed=0; myRightMotorSpeed=50; mySetMotors(); }  // rotate right
-        if (myIncoming == 'G'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=1; myLeftMotorSpeed=50; myRightMotorSpeed=0; mySetMotors(); }  // rotate left
+        if (myIncoming == 'F'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=2; myLeftMotorSpeed=0; myRightMotorSpeed=50; mySetMotors(); }  // F rotate right
+        if (myIncoming == 'G'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=1; myLeftMotorSpeed=50; myRightMotorSpeed=0; mySetMotors(); }  // G rotate left
 
-        if (myIncoming == 'H'){ myLeftMotorSpeed=0; myRightMotorSpeed=0; mySetMotors(); }  // stop
-        if (myIncoming == 'I'){ myLeftMotorSpeed=50; myRightMotorSpeed=50; mySetMotors(); }  // slow
-        if (myIncoming == 'J'){ myLeftMotorSpeed=100; myRightMotorSpeed=100; mySetMotors(); }  // OK
-        if (myIncoming == 'K'){ myLeftMotorSpeed=150; myRightMotorSpeed=150; mySetMotors(); }  // medium
+        if (myIncoming == 'H'){ myLeftMotorSpeed=0; myRightMotorSpeed=0; mySetMotors(); }  // H stop
+        if (myIncoming == 'I'){ myLeftMotorSpeed=50; myRightMotorSpeed=50; mySetMotors(); }  // I slow
+        if (myIncoming == 'J'){ myLeftMotorSpeed=100; myRightMotorSpeed=100; mySetMotors(); }  // J OK
+        if (myIncoming == 'K'){ myLeftMotorSpeed=150; myRightMotorSpeed=150; mySetMotors(); }  // K medium   
 
-        if (myIncoming == 'L'){ myLeftMotorSpeed=200; myRightMotorSpeed=200; mySetMotors(); }  // fast
-        if (myIncoming == 'M'){ myLeftMotorSpeed=255; myRightMotorSpeed=255; mySetMotors(); }  // fastest
+        if (myIncoming == 'L'){ myLeftMotorSpeed=200; myRightMotorSpeed=200; mySetMotors(); }  // L fast
+        if (myIncoming == 'M'){ myLeftMotorSpeed=255; myRightMotorSpeed=255; mySetMotors(); }  // M fastest
 
-        if (myIncoming == 'Q'){ myBrake=0; myLeftMotorSpeed+=10; myRightMotorSpeed-=10; mySetMotors(); }  // left
-        if (myIncoming == 'R'){ myBrake=0; myLeftMotorSpeed+=50; myRightMotorSpeed-=50; mySetMotors(); }  // big left
-        if (myIncoming == 'S'){ myBrake=0; myLeftMotorSpeed-=10; myRightMotorSpeed+=10; mySetMotors(); }  // right
-        if (myIncoming == 'T'){ myBrake=0; myLeftMotorSpeed-=50; myRightMotorSpeed+=50; mySetMotors(); }  // big right
+        if (myIncoming == 'Q'){ myBrake=0; myLeftMotorSpeed+=10;  mySetMotors(); }  // Q left
+        if (myIncoming == 'R'){ myBrake=0; myLeftMotorSpeed+=50;  mySetMotors(); }  // R big left
+        if (myIncoming == 'S'){ myBrake=0; myRightMotorSpeed+=10; mySetMotors(); }  // S right
+        if (myIncoming == 'T'){ myBrake=0; myRightMotorSpeed+=50; mySetMotors(); }  // T big right
 
-        if (myIncoming == 'U'){ myBrake=1; mySetMotors(); }  // coast
-        if (myIncoming == 'V'){ myBrake=2; mySetMotors(); }  // power break
-        if (myIncoming == 'W'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=1; mySetMotors(); }  // set forward
-        if (myIncoming == 'X'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=2; mySetMotors(); }  // set backward
+        if (myIncoming == '1'){ myBrake=0; myLeftMotorSpeed-=10;  mySetMotors(); }  // Q left
+        if (myIncoming == '2'){ myBrake=0; myLeftMotorSpeed-=50;  mySetMotors(); }  // R big left
+        if (myIncoming == '3'){ myBrake=0; myRightMotorSpeed-=10; mySetMotors(); }  // S right
+        if (myIncoming == '4'){ myBrake=0; myRightMotorSpeed-=50; mySetMotors(); }  // T big right
+
+
+        if (myIncoming == 'U'){ myBrake=1; mySetMotors(); }  // U coast
+        if (myIncoming == 'V'){ myBrake=2; mySetMotors(); }  // V power break
+        if (myIncoming == 'X'){ myBrake=0; myLeftMotorDirection=1; myRightMotorDirection=1; mySetMotors(); }  // W set forward
+        if (myIncoming == 'W'){ myBrake=0; myLeftMotorDirection=2; myRightMotorDirection=2; mySetMotors(); }  // X set backward
      
         
         if (myUsbSerialDebugOn){
@@ -188,4 +244,5 @@ void loop() {
       
       }   
     }
+  }   //mygo
 }
