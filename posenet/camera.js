d57a23a78fa450aa08ec9950353db68905d60eab
Jeremy Ellis <keyfreemusic@gmail.com>
2019-02-18 21:31:54 -0800

    Update camera.js

--- posenet/camera.js
+++ posenet/camera.js
@@ -260,6 +260,8 @@ function detectPoseInRealTime(video, net) {
         let myAverageHeight = -1
         
         if ((myRY > 0 ) && (myLY > 0)){
+          console.log(myRY)
+          console.log(myLY)
           myAverageHeight =   (myRY + myLY) / 2.0
           if (myAverageHeight > 0 ){ document.getElementById('myTotalSpeed').value = 200 }
           if (myAverageHeight > 100 ){ document.getElementById('myTotalSpeed').value = 150 }
